4f02a11235cec471aa2d0cf8b2f6f1d7b657a55c
svsannidhay <sannidhayvashalsv@gmail.com>
2021-02-06 03:59:44 +0530

    React-app-created

---
+++ tinder-frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      {/*TinderCards*/}
+      {/*Buttons*/}
+    </div>
+  )
+}
+
+export default App
