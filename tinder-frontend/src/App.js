be24d87e6d0432fdb74aa9f1b40b9bfcbad1d913
svsannidhay <sannidhayvashalsv@gmail.com>
2021-02-06 15:42:00 +0530

    header and tindercards build completed.

--- tinder-frontend/src/App.js
+++ tinder-frontend/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from './Header';
+import TinderCards from './TinderCards';
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      {/*TinderCards*/}
+      <TinderCards/>
       {/*Buttons*/}
     </div>
   )
---
+++ tinder-frontend/src/TinderCards.js
@@ -0,0 +1,50 @@
+import React, {useState} from 'react';
+import TinderCard from 'react-tinder-card';
+import './TinderCards.css';
+
+function TinderCards() {
+
+  const [people, setPeople] = useState([
+    {
+      name: "Elon Musk",
+      url: "https://cdn.vox-cdn.com/thumbor/4QtOwnOxCdwESvt1-CpQSTZvHHA=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19932738/1206292068.jpg.jpg",
+    },
+    {
+      name: "Jeff Bezoz",
+      url : "https://www.imagesource.com/wp-content/uploads/2019/06/Rio.jpg",
+    },
+  ]);
+
+  const swiped = (direction,nameToDelete) => {
+    console.log("removing" + nameToDelete);
+  };
+
+  const outOfFrame = (name) => {
+    console.log(name + "Left the screen!");
+  };
+
+  return (
+    <div className="tinderCards">
+      <div className="tinderCards__cardContainer">
+        {people.map((person) => (
+          <TinderCard
+            className="swipe"
+            key = {person.name}
+            preventSwipe={["up","down"]}
+            onSwipe={(dir)=> swiped(dir,person.name)}
+            onCardLeftScreen={()=> outOfFrame(person.name)}
+          >
+          <div
+            style = {{backgroundImage: `url("${person.url}")`}}
+            className = "card"
+          >
+            <h3>{person.name}</h3>
+          </div>
+          </TinderCard>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default TinderCards;
