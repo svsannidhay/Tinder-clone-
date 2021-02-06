6e2bf681dbebf91fd4173d0997bc7fd2835f3caa
svsannidhay <sannidhayvashalsv@gmail.com>
2021-02-06 21:03:03 +0530

    Front end deployed on firebase and backend on heroku

--- tinder-frontend/src/axios.js
+++ tinder-frontend/src/axios.js
@@ -1,7 +1,8 @@
-import axios from 'axios'
+import axios from 'axios';
+
 
 const instance = axios.create({
-  baseURL: "http://localhost:8001"
+  baseURL: "https://tinderr-backend-clome.herokuapp.com"
 });
 
 export default instance;
